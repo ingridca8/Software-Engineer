a4db0e38bec7ecc7ab5a8ed1adabc715df435a00
ingridca8 <149647031+ingridca8@users.noreply.github.com>
2023-11-13 14:09:45 -0600

    Update linked_lists.py

--- Data_structures/linked_lists.py
+++ Data_structures/linked_lists.py
@@ -1,1 +1,47 @@
+import Node from nodes.py
+#Since we need nodes to create a linked list, we use the class Node that we already have
+
+# We'll be using our Node class
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+# Our LinkedList class
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+  
+# Add your insert_beginning and stringify_list methods below:
+
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+  
+  def stringify_list(self):
+    stringify = str(self.head_node.get_value())
+    while self.head_node.get_next_node() != None:
+      stringify = stringify + '\n' + str(self.head_node.get_next_node().get_value())
+      self.head_node = self.head_node.get_next_node()
+    return stringify
+
+# Test your code by uncommenting the statements below - did your list print to the terminal?
+ll = LinkedList(5)
+ll.insert_beginning(70)
+ll.insert_beginning(5675)
+ll.insert_beginning(90)
+print(ll.stringify_list())
 
